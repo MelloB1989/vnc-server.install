2568a40ff8cf2ac6ad1063f2e70ba09153fe1324
Kartik Deshmukh <63499572+MelloB1989@users.noreply.github.com>
2023-12-12 16:26:29 +0530

    Run in headless mode

--- install.sh
+++ install.sh
@@ -1,15 +1,28 @@
+#!/bin/bash
+
 echo "Hi! I am here to install vnc server!"
 sudo apt update -y
 echo "Installing Desktop Environment..."
 sudo apt install -y xfce4 xfce4-goodies
 sudo apt install -y tightvncserver
-echo "Please input a password"
-echo "Remember this password to connect through vnc"
+
+# Set VNC password
+VNC_PASSWORD="Mellob1989@78"
+mkdir -p "$HOME/.vnc"
+echo $VNC_PASSWORD | vncpasswd -f > "$HOME/.vnc/passwd"
+chmod 600 "$HOME/.vnc/passwd"
+
+echo "Starting VNC Server..."
 vncserver
+
+# Configuring VNC Server
 vncserver -kill :1
 cd && cd /home/ubuntu
 mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
-sudo mv xstartup //home/ubuntu/.vnc
+sudo mv xstartup /home/ubuntu/.vnc
 sudo chmod +x ~/.vnc/xstartup
+
+# Start VNC Server with the new setup
 vncserver
+
 echo "Enjoy! Task Completed!"
