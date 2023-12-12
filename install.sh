dd7f77392352b96f944f463c1cb57a5c48e0cfc8
Kartik Deshmukh <63499572+MelloB1989@users.noreply.github.com>
2023-12-12 19:04:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,25 +4,7 @@ echo "Hi! I am here to install vnc server!"
 sudo apt update -y
 echo "Installing Desktop Environment..."
 sudo apt install -y xfce4 xfce4-goodies
-sudo apt install -y tightvncserver
+sudo apt-get install -y lightdm x11vnc
 
-# Set VNC password
-VNC_PASSWORD="Mellob1989@78"
-mkdir -p "$HOME/.vnc"
-echo $VNC_PASSWORD | vncpasswd -f > "$HOME/.vnc/passwd"
-chmod 600 "$HOME/.vnc/passwd"
-
-echo "Starting VNC Server..."
-vncserver
-
-# Configuring VNC Server
-vncserver -kill :1
-cd && cd /home/coder
-mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
-sudo mv xstartup /home/coder/.vnc
-sudo chmod +x ~/.vnc/xstartup
-
-# Start VNC Server with the new setup
-vncserver
 
 echo "Enjoy! Task Completed!"
