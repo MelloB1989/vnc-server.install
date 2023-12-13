9f6c2b3fa98ebe55e8fc687d047f5d5ca7a53757
Kartik Deshmukh <63499572+MelloB1989@users.noreply.github.com>
2023-12-13 07:31:59 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ export DEBIAN_FRONTEND=noninteractive
 echo "Hi! I am here to install vnc server!"
 sudo apt update -y
 echo "Installing Desktop Environment..."
-sudo apt-get remove -y gdm3
+#sudo apt-get remove -y gdm3
 sudo apt install -y --no-install-recommends xfce4 xfce4-goodies
-sudo apt-get install -y --no-install-recommends lightdm x11vnc
+sudo apt-get install -y --no-install-recommends x11vnc
 sudo cp ./x11vnc.service /lib/systemd/system/x11vnc.service
-sudo cp ./lightdm.conf /etc/lightdm/lightdm.conf.d/lightdm.conf
+#sudo cp ./lightdm.conf /etc/lightdm/lightdm.conf.d/lightdm.conf
 sudo useradd -m avidia
 echo "avidia:avidia" | sudo chpasswd
 sudo usermod -aG sudo avidia
@@ -17,6 +17,6 @@ x11vnc -storepasswd AvidiaVNC ~/.vnc/passwd
 sudo systemctl daemon-reload
 sudo systemctl enable x11vnc.service
 sudo systemctl start x11vnc.service
-sudo systemctl restart lightdm
+#sudo systemctl restart lightdm
 
 echo "Enjoy! Task Completed!"
